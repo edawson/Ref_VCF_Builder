491a7873951e1dedd884ca1f99e84d61aecf69ea
Eric T Dawson <eric.t.dawson@gmail.com>
2015-04-03 19:28:40 -0500

    added smart chrom handling and temp folder

--- make_dbsnp.sh
+++ make_dbsnp.sh
@@ -10,47 +10,64 @@
 link=$1
 base_name=$3
 ref_link=$2
-download=1
+download=0
 index=1
-downloader=./bin/axel
-ref_args=""
-downloader=wget
-twoBitToFa=./bin/twoBitToFa
+downloader=`pwd`/bin/axel
+d_args="-n 5"
+#downloader=wget
+GATK=`pwd`/bin/GenomeAnalysisTK.jar
+PICARD=`pwd`/bin/picard.jar
+samtools=`pwd`/bin/samtools
+twoBitToFa=`pwd`/bin/twoBitToFa
 
 ## download VCF files
 
 files=`curl ${link} 2>&1 | rev | cut -d " " -f 1 | rev | grep "vcf.gz$"`
 if [ "$download" -eq 1 ]
 then
-	for i in $files; 
-	do 
-		$downloader ${link}/$i
-    done
+	mkdir temp
+	cd temp
+	for i in $files;
+	do
+		$downloader ${d_args} ${link}/$i
+  done
+	cd ..
 fi
 
 
 ## unzip the vcf files
-for i in ${files}; do echo "gzip -d $i" >> launchfile.txt; done
-python /bin/LaunChair/launcher.py -i launchfile.txt -c 1
+echo "Let's unzip those files in parallel!"
+for i in ${files}; do echo "gzip -d ./temp/$i" >> launchfile.txt; done
+python ./bin/LaunChair/launcher.py -i launchfile.txt -c 1
+echo "Done."
 
-#Download the reference, generate a fasta index using samtools faidx, and make a dict of the horse reference
-#Download from UCSC
-$downloader ${ref_args} $reflink
-# convert to fasta from 2bit
-ref2bit=basename(${refLink})
-$twoBitToFA ${ref2bit} ${base_name}.ref.fa
-#rename chromosomes
-#sed -i "s/chr//g" horse.fa
+# #Download the reference, generate a fasta index using samtools faidx, and make a dict of the horse reference
+# #Download from UCSC
+# if [ "$download" -eq 1 ]
+# then
+# 	${downloader} ${d_args} $ref_link
+# fi
+# # convert to fasta from 2bit
+# echo "Converting from 2bit to fasta"
+# ref2bit=`basename ${ref_link}`
+# $twoBitToFa ${ref2bit} ${base_name}.ref.fa
+
+#rename chromosomes if necessary
+notChr=`head -n 100 temp/vcf_chr_1.vcf | grep -v "#" | grep "CHR\|chr" | wc -l`
+if [ ${notChr} -eq 0 ]
+then
+	sed -i 's/chr//g' ${base_name}.ref.fa
+fi
 #Generate a fasta index
-samtools faidx ${base_name}.ref.fa
+$samtools faidx ${base_name}.ref.fa
 ## Create fasta dict
 java -jar $PICARD CreateSequenceDictionary R=${base_name}.ref.fa O=${base_name}.ref.dict
 
 ## Run GATK to combine the VCFS
-for i in `ls temp`; do echo "--variant $i \\"; done > temp.txt 
+for i in `ls temp`; do echo "--variant ./temp/$i"; done > temp.txt
 java -Xmx12g -jar $GATK -T CombineVariants -nt 2 \
 -R ${base_name}.ref.fa \
---out horse_SNP.vcf \
+--out ${base_name}.vcf \
 `cat temp.txt`
 
-rm -rf temp.txt temp
+#rm -rf temp.txt temp launchfile.txt
