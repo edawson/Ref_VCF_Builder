96b7966926cae7f0776cc64ff3309cbd7b77b989
Eric T Dawson <eric.t.dawson@gmail.com>
2015-04-27 13:01:34 -0500

    Fixed missing bin files

---
+++ bin/LaunChair/LaunChair.py
@@ -0,0 +1,45 @@
+__author__ = 'Eric T Dawson'
+import subprocess
+import multiprocessing as mp
+import os
+
+
+def func(task):
+    # subprocess.Popen(task, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
+    try:
+        subprocess.call(task, shell=True)
+    except KeyboardInterrupt, Exception:
+        raise KeyboardInterrupt
+    return
+
+class LaunChair:
+    def __init__(self):
+        self.jobfile = None
+        self.work = None
+        return
+
+    def set_file(self, jobfile):
+        self.jobfile = jobfile
+        self.set_work()
+
+    def set_work(self):
+        work = []
+        with open(self.jobfile, "r") as fi:
+            for line in fi:
+                work.append(line)
+        self.work = work
+
+    def run(self, cores_per_task):
+        n_cpus = mp.cpu_count()
+        if cores_per_task > n_cpus:
+            raise ValueError("The number of cores per task must be less than the number of CPUs.")
+        size = n_cpus / cores_per_task
+
+        p = mp.Pool(size)
+        try:
+            ret = p.map_async(func, self.work).get(9999)
+        except KeyboardInterrupt:
+            p.terminate()
+            exit(1)
+
+        return
---
+++ bin/LaunChair/launcher.py
@@ -0,0 +1,19 @@
+__author__ = 'Eric T Dawson'
+
+import argparse
+import sys
+from LaunChair import LaunChair
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "-infile", dest="infile", type=str,required=True, default=sys.stdin)
+    parser.add_argument("-c", "-cores", dest="cores", type=int, default=1, required=False)
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    l = LaunChair()
+    l.set_file(args.infile)
+    l.run(args.cores)
+
